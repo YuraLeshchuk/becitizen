cf7a325fb1c579becd2db90f7e82e30fd846a806
Yurii Leshchuk <ylesh@softserveinc.com>
2020-09-22 14:27:56 +0300

    content pages are added

---
+++ index.js
@@ -0,0 +1,17 @@
+// main page buttons redirects
+
+document.getElementById("airbutton").onclick = function () {
+    location.href = "air.html";
+};
+document.getElementById("waterbutton").onclick = function () {
+    location.href = "water.html";
+};
+document.getElementById("forestsbutton").onclick = function () {
+    location.href = "forests.html";
+};
+document.getElementById("warmingbutton").onclick = function () {
+    location.href = "warming.html";
+};
+document.getElementById("plasticbutton").onclick = function () {
+    location.href = "plastic.html";
+};
